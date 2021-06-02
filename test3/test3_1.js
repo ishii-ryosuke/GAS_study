58b8c89b92be29c24f818b6ae5757bae618856cf
ishii-ryosuke <ryosuke.ishii@esm.co.jp>
2021-06-02 15:46:02 +0900

    課題3_1解答

---
+++ test3/test3_1.js
@@ -0,0 +1,13 @@
+/**
+ * [3-1]
+ * 以下の関数を作成して、作成しした関数を呼び出してください。
+ * 呼び出す際に引数に 1 を渡し、その戻り値をコンソールログに表示してください。
+ * ・仮引数の値を10倍にして呼び出し元に返してください。
+ */
+function main() {
+    console.log(tenTimeMethod(1));
+}
+
+function tenTimeMethod(value) {
+    return value * 10;
+}
