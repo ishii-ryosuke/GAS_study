171681a8ce0e851a38087b2415b52417703152d9
ishii-ryosuke <ryosuke.ishii@esm.co.jp>
2021-06-02 10:55:33 +0900

    課題1-7解答

---
+++ test1/test1_7.js
@@ -0,0 +1,13 @@
+/**
+ * [1-7]
+ * 以下の内容の関数を作成してください。
+ * (1) 以下の配列を宣言してください。
+ * 　['japanese', 'math', 'english']
+ * (2) (1) で宣言した配列の3番目の要素を 'science' に変更してください。
+ * (3) (2) で変更した配列をコンソールログに表示してください。
+ */
+function printArrayChangedThirdElement() {
+    var subject = ['japanese', 'math', 'english'];
+    subject[2] = 'science'
+    console.log(subject);
+}
